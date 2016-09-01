bd095d3ded3de4ce2686ece9d06cf7a3f3372b03
Anders Friis <Friis73071@gmail.com>
2016-09-01 14:13:01 -0400

    Mark down and class in py

---
+++ word_adventure.py
@@ -0,0 +1,33 @@
+name = raw_input("What is your name? ")
+print "Hello, %s." % name
+
+class Character:
+	def __init__(self, name, health):
+  		self.name = ""
+  		self.health = health
+
+
+class Room:( self, doors)
+	def __init__
+		self.doors = 3
+
+
+class Enemy:
+    def __init__(self, name, health, damage):
+        self.name = name
+        self.health = health
+        self.damage = damage
+ 
+    def is_alive(self):
+        return self.health > 0
+
+class Gameplay: (self, right, left, forward)
+	def __init__
+	self.turn = right
+	self.turn = left
+	self.turn = forward
+
+	
+
+name = raw_input("What is your name? ")
+print "Hello, %s. + "Let's play" " % name
