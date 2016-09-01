4295a3f757060c4fa523eb493070ef75dd955ed9
Anders Friis <Friis73071@gmail.com>
2016-09-01 17:14:41 -0400

    add left, right doors with quiz

--- word_adventure.py
+++ word_adventure.py
@@ -1,5 +1,5 @@
-name = raw_input("What is your name? ")
-print "Hello, %s." % name
+name = input('What is your name?\n')
+print ('Get ready to play, %s.' % name)
 
 class Character:
 	def __init__(self, name, health):
@@ -7,9 +7,18 @@ class Character:
   		self.health = health
 
 
-class Room:( self, doors)
-	def __init__
+class Room:
+	def __init__(self):
 		self.doors = 3
+		self.right = "right"
+		self.left = "left"
+		self. straight = "straight"
+
+class Puzzle:
+	def __init__(self, puzzle1, puzzle2, puzzle3):
+		self.puzzle1 
+		self.puzzle2
+		self.puzzle3
 
 
 class Enemy:
@@ -17,17 +26,79 @@ class Enemy:
         self.name = name
         self.health = health
         self.damage = damage
+        self. enemy
  
     def is_alive(self):
         return self.health > 0
 
-class Gameplay: (self, right, left, forward)
-	def __init__
-	self.turn = right
-	self.turn = left
-	self.turn = forward
 
+
+def puzzle1():
 	
+	puzzle_answer = input (" What was the heighest mountain in the world before MT Everest was discovered?")
+	if puzzle_answer == "mt everest":
+		print ("The door opens and you step into a new room")
+	else: 
+		print ("The answer is not correct, try again")
 
-name = raw_input("What is your name? ")
-print "Hello, %s. + "Let's play" " % name
+		puzzle1()
+## Note to self, do not put anythin else under else statement with puzzle1()
+def puzzle2():
+	
+	puzzle_answer = input (" Cry Baby that plays for Real Madrid?")
+	if puzzle_answer == "Ronaldo":
+			print ("The door opens and you step into a new room")
+	else: 
+		print ("The answer is not correct, try again")
+
+		puzzle2()
+
+
+		
+
+
+
+Start = Room
+print (" You just woke up and find yourself in a dark house. All the windows are blocked and sealed. There are 3 doors in the room.")
+
+
+
+
+answer = input ("What door will you Pick? Right, Left or Straight?")
+
+
+if Room().right == answer:
+    print ("You try to open the door, but the door slams in your face. A voice appears and says: I will open this door if you answer this question correctly")
+    puzzle1()
+
+
+if Room().left == answer:
+    print ("You try to open the door, but the door slams in your face. A voice appears and says: I will open this door if you answer this question correctly")
+    puzzle2()
+
+else:
+	print ("You are facing a ghost enemy")
+
+def enemy ()
+
+	enemy = 
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
